518e787d5d02477239749b425e701affbab8d9f4
JeppeSigaard <jeppe@smatmonkey.dk>
2015-08-13 14:31:46 +0200

    Opsat sidestruktur med index.php som route

--- index.php
+++ index.php
@@ -1,2 +1,26 @@
-<?php get_header(); ?>
-<?php get_footer(); ?>
+<?php 
+
+get_header();
+
+
+// Forside eller side markeret som forside
+if(is_home() || is_front_page()){
+    require 'pages/front-page.php';
+}
+
+elseif(is_archive()){
+    require 'pages/archive.php';
+}
+
+elseif(is_page()){
+    require 'pages/page.php';
+}
+
+elseif(is_single()){
+    require 'pages/single.php';
+}
+
+
+
+else{require 'pages/404.php';}
+get_footer();
