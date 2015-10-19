eacfa41ae76c1e734ac1191da419b6973c33e40a
JeppeSigaard <jeppe@smatmonkey.dk>
2015-10-19 10:00:59 +0200

    indsat analytics

--- header.php
+++ header.php
@@ -5,6 +5,15 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <title><?php wp_title(' · ', true, 'right'); ?></title>
     <?php wp_head(); ?>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-68531376-1', 'auto');
+        ga('send', 'pageview');
+    </script>
 </head>
 <body <?php body_class(); ?>>
     <header class="site-header">
